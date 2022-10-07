898215c1b87671ee74510404c1f8dce6d99e138f
DMITRY <d.stasenko@mirtekgroup.ru>
2022-10-07 20:37:28 +0300

    Refactoring name of methods, delete useless functions connect/disconnect

--- main.py
+++ main.py
@@ -60,37 +60,35 @@ class Ui(QtWidgets.QMainWindow):
         self.labels_dict = {self.textEdit_hex: self.label_hex, self.textEdit_dec: self.label_dec, self.textEdit_bin: self.label_bin}
         # list text fields to switching select current text field
         self.inputs_list = [self.textEdit_bin, self.textEdit_dec, self.textEdit_hex]
-        # connecting number-buttons
-        self.all_num_signals_connect()
         # connecting select empty
-        self.textEdit_bin.selectionChanged.connect(self.select_inp_bin)
-        self.textEdit_dec.selectionChanged.connect(self.select_inp_dec)
-        self.textEdit_hex.selectionChanged.connect(self.select_inp_hex)
+        self.textEdit_bin.selectionChanged.connect(self.select_binary_field)
+        self.textEdit_dec.selectionChanged.connect(self.select_decimal_field)
+        self.textEdit_hex.selectionChanged.connect(self.select_hexadecimal_field)
         self.textEdit_bin.selectionChanged.connect(self.paint_textFields)
         self.textEdit_dec.selectionChanged.connect(self.paint_textFields)
         self.textEdit_hex.selectionChanged.connect(self.paint_textFields)
         # signals ariphmetic buttons connection
-        self.pushButton_sum.clicked.connect(self.ariphmetic_create(add))
-        self.pushButton_sub.clicked.connect(self.ariphmetic_create(sub))
-        self.pushButton_mul.clicked.connect(self.ariphmetic_create(mul))
-        self.pushButton_div.clicked.connect(self.ariphmetic_create(floordiv))
-        self.pushButton_result.clicked.connect(self.ariphmetic_create(None))
+        self.pushButton_sum.clicked.connect(self.create_arifmetic_functions(add))
+        self.pushButton_sub.clicked.connect(self.create_arifmetic_functions(sub))
+        self.pushButton_mul.clicked.connect(self.create_arifmetic_functions(mul))
+        self.pushButton_div.clicked.connect(self.create_arifmetic_functions(floordiv))
+        self.pushButton_result.clicked.connect(self.create_arifmetic_functions(None))
         # signals buttons C and CE
-        self.pushButton_clear.clicked.connect(self.clk_C_btn)
-        self.pushButton_clear_entry.clicked.connect(self.clk_CE_btn)
+        self.pushButton_clear.clicked.connect(self.click_C)
+        self.pushButton_clear_entry.clicked.connect(self.click_CE)
         # pointer to one of three text fields objects
         self.current_handler = self.textEdit_dec
         # start clearing select field
-        self.current_handler.setPlainText("0")
+        self.output_to_all_fields(0)
         # connected left anr right shift
-        self.pushButton_lshift.clicked.connect(self.clk_lshift_btn)
-        self.pushButton_rshift.clicked.connect(self.clk_rshift_btn)
+        self.pushButton_lshift.clicked.connect(self.click_leftShift)
+        self.pushButton_rshift.clicked.connect(self.click_rightShift)
         # init functions dict to match with button signals
-        self.functions_dict = {i: self.clk_evt_btn_create(i) for i in "0123456789ABCDEF"}
+        self.functions_dict = {i: self.click_button_event_create(i) for i in "0123456789ABCDEF"}
         # add entries of arifmetic operations
-        self.functions_dict.update({key: self.ariphmetic_create(op) for op, key in self.operations_dict.items()})
+        self.functions_dict.update({key: self.create_arifmetic_functions(op) for op, key in self.operations_dict.items()})
         # add entries of other keys
-        self.functions_dict.update({"clear": self.clk_C_btn, "clear_entry":self.clk_CE_btn, "backspace":self.clk_backspace_btn, "rshift":self.clk_rshift_btn, "lshift":self.clk_lshift_btn})
+        self.functions_dict.update({"clear": self.click_C, "clear_entry":self.click_CE, "backspace":self.click_backspace, "rshift":self.click_rightShift, "lshift":self.click_leftShift})
         # stand on of active field
         self.paint_textFields()
 
@@ -134,100 +132,72 @@ class Ui(QtWidgets.QMainWindow):
             else:
                 label.setPalette(self.palInactive)
 
-    def clk_evt_btn_create(self, num):
+    def click_button_event_create(self, character):
         """creating"""
-        def clk_evt_btn():
+        def click_button_event():
             if self.new_write_flag == True:
                 self.new_write_flag = False
-                itext = self.recieve_value(str(num))
+                int_number = self.convert_string_to_int(character)
             else:
-                text = self.current_handler.toPlainText()
-                itext = self.recieve_value(text + str(num))
-            self.set_all_notation(itext)
-        return clk_evt_btn
+                existing_text = self.current_handler.toPlainText()
+                int_number = self.convert_string_to_int(existing_text + character)
+            self.output_to_all_fields(int_number)
+        return click_button_event
 
-    def ariphmetic_create(self, operator):
+    def create_arifmetic_functions(self, operator):
         """creating functions from arifmetic buttons"""
-        def ar_handler():
-            self.aripfmetic_operations(operator)
-        return ar_handler
+        def arifmetic_function():
+            self.perform_aripfmetic_operation(operator)
+        return arifmetic_function
 
-    def recieve_value(self, input):
+    def convert_string_to_int(self, string_number):
         """replace separators in string, convert non-decimal value to decimal and return int"""
-        input = input.replace(' ', '')
+        string_number = string_number.replace(' ', '')
         try:
             if self.current_handler == self.textEdit_bin:
-                itext = int(input, base=2)
+                int_number = int(string_number, base=2)
             if self.current_handler == self.textEdit_hex:
-                itext = int(input, base=16)
+                int_number = int(string_number, base=16)
             if self.current_handler == self.textEdit_dec:
-                itext = int(input, base=10)
-            return itext
+                int_number = int(string_number, base=10)
+            return int_number
         except ValueError:
-            itext = 0
+            int_number = 0
             return 0
 
-    def all_num_signals_connect(self):
-        """connect all signals of text entries"""
-        self.textEdit_dec.textChanged.connect(self.output_update)
-        self.textEdit_bin.textChanged.connect(self.output_update)
-        self.textEdit_hex.textChanged.connect(self.output_update)
-
-    def all_num_signals_disconnect(self):
-        """disconnect all signals of text entries"""
-        self.textEdit_dec.textChanged.disconnect(self.output_update)
-        self.textEdit_bin.textChanged.disconnect(self.output_update)
-        self.textEdit_hex.textChanged.disconnect(self.output_update)
-
-    def output_update(self):
+    def update_fields(self):
         """get current number in text-editor and add input num from buttons to the end"""
-        text = self.current_handler.toPlainText()
-        itext = self.recieve_value(text)
-        self.select_notation_and_set(itext)
+        existing_text = self.current_handler.toPlainText()
+        int_number = self.convert_string_to_int(existing_text)
+        self.output_to_all_fields(int_number)
 
-    def select_notation_and_set(self, itext):
-        """set values to not-current editors"""
-        self.all_num_signals_disconnect()
-        if self.current_handler != self.textEdit_dec: self.set_dec(itext)
-        if self.current_handler != self.textEdit_bin: self.set_bin(itext)
-        if self.current_handler != self.textEdit_hex: self.set_hex(itext)
-        self.all_num_signals_connect()
-
-    def set_current_notation(self, itext):
-        """set values to current editor"""
-        self.all_num_signals_disconnect()
-        if self.current_handler == self.textEdit_dec: self.set_dec(itext)
-        if self.current_handler == self.textEdit_bin: self.set_bin(itext)
-        if self.current_handler == self.textEdit_hex: self.set_hex(itext)
-        self.all_num_signals_connect()
-
-    def set_all_notation(self, itext):
+    def output_to_all_fields(self, int_number):
         """set valuet to all editors"""
-        self.all_num_signals_disconnect()
-        self.set_dec(itext)
-        self.set_bin(itext)
-        self.set_hex(itext)
-        self.all_num_signals_connect()
+        # self.all_num_signals_disconnect()
+        self.print_decimal(int_number)
+        self.print_binary(int_number)
+        self.print_hexadecimal(int_number)
+        # self.all_num_signals_connect()
 
-    def set_dec(self, itext):
+    def print_decimal(self, itext):
         """output decimal-formating text"""
         self.textEdit_dec.setPlainText(f"{itext}")
 
-    def set_bin(self, itext):
+    def print_binary(self, itext):
         """output binary-formating text"""
         sout = f"{itext:032b}"
         self.textEdit_bin.setPlainText(f"{sout[0:8]} {sout[8:16]} {sout[16:24]} {sout[24:32]}")
 
-    def set_hex(self, itext):
+    def print_hexadecimal(self, itext):
         """output hexadecimal-formating text"""
         sout = f"{itext:08X}"
         self.textEdit_hex.setPlainText(f"{sout[:2]} {sout[2:4]} {sout[4:6]} {sout[6:]}")
 
-    def aripfmetic_operations(self, operation):
+    def perform_aripfmetic_operation(self, operation):
         """connection with signal clicked of buttons sum, sub, mul, dev, result"""
         # get text by active input entry and convert to int
         text = self.current_handler.toPlainText()
-        itext = self.recieve_value(text)
+        itext = self.convert_string_to_int(text)
         # first input number need to pull on result and not execute arifmetic operations
         if not self.arifmetic_result:
             self.arifmetic_result = itext
@@ -240,53 +210,53 @@ class Ui(QtWidgets.QMainWindow):
 
         if self.current_ariphmetic_operation is None:
             self.text_log += str(self.arifmetic_result) + "\n"
-            self.set_all_notation(self.arifmetic_result)
+            self.output_to_all_fields(self.arifmetic_result)
             self.arifmetic_result = None
 
         self.new_write_flag = True
         self.textEdit_log.setPlainText(self.text_log)
 
-    def select_inp_bin(self):
+    def select_binary_field(self):
         """connection with signal selectChange of decimal entry"""
         self.current_handler = self.textEdit_bin
-    def select_inp_dec(self):
+    def select_decimal_field(self):
         """connection with signal selectChange of binary entry"""
         self.current_handler = self.textEdit_dec
-    def select_inp_hex(self):
+    def select_hexadecimal_field(self):
         """connection with signal selectChange of hex entry"""
         self.current_handler = self.textEdit_hex
 
-    def clk_C_btn(self):
+    def click_C(self):
         """connection with signal of C-button"""
         self.arifmetic_result = None
         self.current_ariphmetic_operation = None
         self.text_log = ""
         self.textEdit_log.setPlainText(self.text_log)
-        self.clk_CE_btn()
+        self.click_CE()
 
-    def clk_CE_btn(self):
+    def click_CE(self):
         """connection with signal of CE-button"""
         self.new_write_flag = True
-        self.set_all_notation(0)
+        self.output_to_all_fields(0)
 
-    def clk_backspace_btn(self):
+    def click_backspace(self):
         """connection with signal of backspace-button"""
         text = self.current_handler.toPlainText()
         text = text[:-1]
-        itext = self.recieve_value(text)
-        self.set_all_notation(itext)
+        itext = self.convert_string_to_int(text)
+        self.output_to_all_fields(itext)
 
-    def clk_lshift_btn(self):
+    def click_leftShift(self):
         itext = int(self.current_handler.toPlainText())
         itext <<= 1
         itext &= 0xFFFFFFFF
-        self.set_all_notation(itext)
+        self.output_to_all_fields(itext)
 
-    def clk_rshift_btn(self):
+    def click_rightShift(self):
         itext = int(self.current_handler.toPlainText())
         itext >>= 1
         itext &= 0xFFFFFFFF
-        self.set_all_notation(itext)
+        self.output_to_all_fields(itext)
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
